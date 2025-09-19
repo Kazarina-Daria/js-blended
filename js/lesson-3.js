9f23a575ce484e460c4ed8de2a4a6fe2059e2e76
Daria Kazarina <kdasa033@gmail.com>
2025-09-19 22:24:50 +0200

    lesson 3

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -297,3 +297,20 @@
 // console.log(speedSensorBmw(SPEED))
 
 // console.log(speedSensor.call(audi, SPEED));
+
+
+
+//Call method
+function greet(str) { 
+  console.log(`${str}, ${this.username}, your room is ${this.room}!`);} 
+ 
+const mango = { 
+  username: "Mango", 
+	room: 27 
+}; 
+ 
+const poly = { 
+  username: "Poly", 
+	room: 191}; 
+greet.call(mango, "Welcome"); // "Welcome, Mango, your room is 27!" 
+greet.call(poly, "Aloha"); // "Aloha, Poly, your room is 191!" 
